b16613abef4d90d190de46299874a0e66b28c9dc
SZETO Ngai man <115551742+SheldonCTO@users.noreply.github.com>
2024-04-15 19:43:09 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,36 +6,35 @@ const jwt = require('jsonwebtoken');
 const passport = require("passport");
 const passportJWT = require("passport-jwt");
 const app = express();
+require('dotenv').config();
 
+// const HTTP_PORT = process.env.PORT || 8080;
 const HTTP_PORT = process.env.PORT || 8080;
 
+
 // JSON Web Token Setup
 let ExtractJwt = passportJWT.ExtractJwt;
 let JwtStrategy = passportJWT.Strategy;
-
-// Configure its options
 let jwtOptions = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'),
-  secretOrKey: '&0y7$noP#5rt99&GB%Pz7j2b1vkzaB0RKs%^N^0zOP89NT04mPuaM!&G8cbNZOtH',
+  secretOrKey: process.env.JWT_SECRET
 };
+// Configure its options
+// let jwtOptions = {
+//   jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'),
+//   secretOrKey: '&0y7$noP#5rt99&GB%Pz7j2b1vkzaB0RKs%^N^0zOP89NT04mPuaM!&G8cbNZOtH',
+// };
 
-// JSON Web Token Setup
-let ExtractJwt = passportJWT.ExtractJwt;
-let JwtStrategy = passportJWT.Strategy;
-
-
+// IMPORTANT - this secret should be a long, unguessable string
+// (ideally stored in a "protected storage" area on the web server).
+// We suggest that you generate a random 50-character string
+// using the following online tool:
+// https://lastpass.com/generatepassword.php
 
 let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
     console.log('payload received', jwt_payload);
-
     if (jwt_payload) {
-        // The following will ensure that all routes using 
-        // passport.authenticate have a req.user._id, req.user.userName, req.user.fullName & req.user.role values 
-        // that matches the request payload data
-        next(null, { _id: jwt_payload._id, 
-            userName: jwt_payload.userName, 
-            fullName: jwt_payload.fullName, 
-            role: jwt_payload.role }); 
+        next(null, { _id: jwt_payload._id, userName: jwt_payload.userName, fullName: jwt_payload.fullName, role: jwt_payload.role });
     } else {
         next(null, false);
     }
